90e70b31a81c5baabb3a5c2098e0561949c7a810
Navneet Sheoran <navneetsheoran537@gmail.com>
2024-06-04 12:28:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quezgame/quezData.java
@@ -0,0 +1,87 @@
+package com.example.quezgame;
+
+public class quezData {
+
+    String[] questions = {
+            "Who is the Father of our Nation?",
+            "Who was the first President of India?",
+            "Who is known as the Father of the Indian Constitution?",
+            "Which is the most sensitive organ in our body?",
+            "Giddha is the folk dance of which state?",
+            "Who was the first Prime Minister of India?",
+            "Which is the heavier metal of these two? Gold or Silver?",
+            "Who invented the computer?",
+            "How many Kilobytes are equal to 1 Megabyte (MB)?",
+            "Which component is known as the 'brain' of a computer?"
+    };
+
+    String[] optionA = {
+            "Jawaharlal Nehru",
+            "Dr.B.R. Ambedkar",
+            "Mahatma Gandhi",
+            "Eyes",
+            "Gujarat",
+            "Jawaharlal Nehru",
+            "Gold",
+            "Bill Gates",
+            "512 Kilobytes",
+            "CPU"
+
+    };
+
+    String[] optionB = {
+            "Mahatma Gandhi",
+            "Jawaharlal Nehru",
+            "Subhash Chandra Bose",
+            "Ears",
+            "Rajasthan",
+            "Indira Gandhi",
+            "Silver",
+            "Steve Jobs",
+            "1024 Kilobytes",
+            "RAM"
+
+            };
+
+    String[] optionC = {
+            "Subhash Chandra Bose",
+            "Dr. Rajendra Prasad",
+            "Dr.B.R. Ambedkar",
+            "Nose",
+            "Punjab",
+            "Atal Bihari Vajpayee",
+            "They have the same weight",
+            "Alan Turing",
+            "2048 Kilobytes",
+            "GPU"
+
+    };
+
+    String[] optionD = {
+            "Sardar Vallabhbhai Patel",
+            "Indira Gandhi",
+            "Jawaharlal Nehru",
+            "Skin",
+            "Kerala",
+            "Narendra Modi",
+            "It depends on the quantity",
+            "Charles Babbage",
+            "4096 Kilobytes",
+            "Hard Drive"
+    };
+
+    String[] correctAnswers = {
+            "Mahatma Gandhi",
+            "Dr. Rajendra Prasad",
+            "Dr.B.R. Ambedkar",
+            "Skin",
+            "Punjab",
+            "Jawaharlal Nehru",
+            "Gold",
+            "Charles Babbage",
+            "1024 Kilobytes",
+            "CPU"
+    };
+
+
+}
